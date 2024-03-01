af44f0d91108e7e152110dea512238e8575ca071
demivlkv <demi.h@me.com>
2024-03-01 09:44:43 -0800

    create Container component

---
+++ components/Container/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native';
+
+const Container = () => {
+  return (
+    <View>
+      Container
+    </View>
+  );
+};
+
+export default Container;
