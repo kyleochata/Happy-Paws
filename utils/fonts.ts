f2eef14b6217382c1d8292a29899aeb5f55be9db
demivlkv <demi.h@me.com>
2024-03-01 20:00:19 -0800

    polish footer; add fira sans

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -2,12 +2,18 @@ import * as Font from 'expo-font'
 
 // Define the font family name
 export const Montserrat = 'Montserrat'
+export const MontserratBold = 'MontserratBold'
+export const FiraSans = 'FiraSans'
+export const FiraSansBold = 'FiraSansBold'
 
 // Load custom font synchronously
 export const loadCustomFont = async () => {
   await Font.loadAsync({
     [Montserrat]: require('../assets/fonts/Montserrat/static/Montserrat-Regular.ttf'),
     // You can add more custom fonts here if needed
+    [MontserratBold]: require('../assets/fonts/Montserrat/static/Montserrat-Bold.ttf'),
+    [FiraSans]: require('../assets/fonts/FiraSans/static/FiraSans-Regular.ttf'),
+    [FiraSansBold]: require('../assets/fonts/FiraSans/static/FiraSans-Bold.ttf'),
   })
 }
 
