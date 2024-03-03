dfd515988afe8130ba3be0e2cb9d41a0a0f41f08
demivlkv <demi.h@me.com>
2024-03-02 16:31:30 -0800

    set up initial styling

--- components/PhotoGallery/style.ts
+++ components/PhotoGallery/style.ts
@@ -3,7 +3,45 @@ import { Montserrat, FiraSans } from '../../utils/fonts'
 import { colors } from '../../utils/constants';
 
 const styles = StyleSheet.create({
+  mobileh1: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 36,
+    color: colors.black,
+  },
+  mobileText: {
+    fontFamily: FiraSans,
+    fontSize: 20,
+    color: colors.black,
+  },
+  mobileViewPhotosBtn: {
 
+  },
+  mobileArrowBtn: {
+
+  },
+  mobileCarousel: {
+
+  },
+
+  webh1: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 36,
+    color: colors.black,
+  },
+  webText: {
+    fontFamily: FiraSans,
+    fontSize: 20,
+    color: colors.black,
+  },
+  webViewPhotosBtn: {
+
+  },
+  webArrowBtn: {
+
+  },
+  webCarousel: {
+
+  },
 });
 
 export default styles;
