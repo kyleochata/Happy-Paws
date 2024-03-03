80165a690c43038d7f72f6d3d128611fa4c0e7fa
demivlkv <demi.h@me.com>
2024-03-03 09:16:50 -0800

    set up skeleton for BookAService component

---
+++ components/BookAService/style.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import { Montserrat, FiraSans } from '../../utils/fonts';
+import { colors } from '../../utils/constants';
+
+const styles = StyleSheet.create({
+
+});
+
+export default styles;
