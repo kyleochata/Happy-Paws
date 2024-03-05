60fc383034178d4da9eacb9586389b17241d4bc7
demivlkv <demi.h@me.com>
2024-03-04 23:04:29 -0800

    i'm donezoooo

--- components/Services/CatServiceCards.tsx
+++ components/Services/CatServiceCards.tsx
@@ -27,106 +27,118 @@ const CatServiceCards = ({ boardingService, daycareService, groomingService }: C
   const mobile = Platform.OS !== 'web';
 
   return (
-    <View style={mobile ? styles.mobServicesForPetWrapper : styles.webServicesForPetWrapper}>
+    <View>
       {/* OVERNIGHT BOARDING */}
-      <View style={mobile ? styles.mobCard : styles.webCard}>
-        <Text style={mobile ? styles.mobLabel : styles.webLabel}>
-          Overnight Boarding
-        </Text>
-        <View style={mobile ? styles.mobServices : styles.webServices}>
-          {/* <Text style={mobile ? styles.mobH3 : styles.webH3}>
-            {boardingService?.title}: {'$'}{boardingService?.price} per night
-          </Text>
-          <Text style={mobile ? styles.mobDescription : styles.webDescription}>
-            {boardingService?.description}
-          </Text> */}
-          {boardingService?.map((service) => (
-            <>
-              <Text style={mobile ? styles.mobH3 : styles.webH3}>
-                {service.title}: {'$'}{service.price} per night
-              </Text>
-              <Text style={mobile ? styles.mobDescription : styles.webDescription}>
-                {service.description}
-              </Text>
-            </>
-          ))}
-        </View>
-        <View>
+      <View style={mobile ? styles.mobServicesForPetWrapper : styles.webServicesForPetWrapper}>
+        <View style={mobile ? styles.mobCard : styles.webCard}>
+          <View style={mobile ? styles.mobServices : styles.webServices}>
+            <Text style={mobile ? styles.mobLabel : styles.webLabel}>
+              Overnight Boarding
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Standard: {'$'}30 per night
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes a comfortable sleeping area, basic meals, and access to outdoor play areas.
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Deluxe: {'$'}50 per night
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes a spacious sleeping area, premium meals, access to indoor and outdoor play areas, and webcam access for pet parents to check in.
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Premium: {'$'}70 per night
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes a luxury suite with plush bedding, gourmet meals, personalized attention from staff, daily enrichment activities, and additional amenities such as toys or scratching posts.
+            </Text>
+          </View>
+          <View style={styles.imgWrapper}>
             <Image
               source={require('../../assets/images/service/cat-boarding.jpg')}
               style={mobile ? styles.mobImage : styles.webImage}
             />
-        </View>
-        <View style={mobile ? styles.mobButton : styles.webButton}>
-          <SubmitButton value='Book Now' />
+          </View>
+          <View style={mobile ? styles.mobButton : styles.webButton}>
+            <SubmitButton value='Book Now' />
+          </View>
         </View>
       </View>
 
       {/* DAYCARE */}
-      <View style={mobile ? styles.mobCard : styles.webCard}>
-        <Text style={mobile ? styles.mobLabel : styles.webLabel}>
-          Daycare
-        </Text>
-        <View style={mobile ? styles.mobServices : styles.webServices}>
-        {daycareService?.map((service) => (
-            <>
-              <Text style={mobile ? styles.mobH3 : styles.webH3}>
-                {service.title}: {'$'}{service.price} per night
-              </Text>
-              <Text style={mobile ? styles.mobDescription : styles.webDescription}>
-                {service.description}
-              </Text>
-            </>
-          ))}
-          {/* <Text style={mobile ? styles.mobH3 : styles.webH3}>
-            {daycareService?.title}: {'$'}{daycareService?.price} per day
-          </Text>
-          <Text style={mobile ? styles.mobDescription : styles.webDescription}>
-            {daycareService?.description}
-          </Text> */}
-        </View>
-        <View>
-          <Image
-            source={require('../../assets/images/service/cat-daycare.jpg')}
-            style={mobile ? styles.mobImage : styles.webImage}
-          />
-        </View>
-        <View style={mobile ? styles.mobButton : styles.webButton}>
-          <SubmitButton value='Book Now' />
+      <View style={mobile ? styles.mobServicesForPetWrapper : styles.webServicesForPetWrapper}>
+        <View style={mobile ? styles.mobCard : styles.webCard}>
+          <View style={styles.imgWrapper}>
+            <Image
+              source={require('../../assets/images/service/cat-daycare.jpg')}
+              style={mobile ? styles.mobImage : styles.webImage}
+            />
+          </View>
+          <View style={mobile ? styles.mobServices : styles.webServices}>
+            <Text style={mobile ? styles.mobLabel : styles.webLabel}>
+              Daycare
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Basic: {'$'}20 per day
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes supervised play sessions and basic amenities.
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Standard: {'$'}30 per day
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes supervised play sessions, access to indoor and outdoor play areas, and optional add-ons such as interactive toys or climbing structures.
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Premium: {'$'}45 per day
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes premium supervised play sessions with enrichment activities, specialized attention from staff, and additional amenities such as gourmet treats or cozy hideaways.
+            </Text>
+          </View>
+          <View style={mobile ? styles.mobButton : styles.webButton}>
+            <SubmitButton value='Book Now' />
+          </View>
         </View>
       </View>
 
       {/* GROOMING */}
-      <View style={mobile ? styles.mobCard : styles.webCard}>
-        <Text style={mobile ? styles.mobLabel : styles.webLabel}>
-          Grooming
-        </Text>
-        <View style={mobile ? styles.mobServices : styles.webServices}>
-          {groomingService?.map((service) => (
-            <>
-              <Text style={mobile ? styles.mobH3 : styles.webH3}>
-                {service.title}: {'$'}{service.price} per night
-              </Text>
-              <Text style={mobile ? styles.mobDescription : styles.webDescription}>
-                {service.description}
-              </Text>
-            </>
-          ))}
-          {/* <Text style={mobile ? styles.mobH3 : styles.webH3}>
-            {groomingService?.title}: {'$'}{groomingService?.price}
-          </Text>
-          <Text style={mobile ? styles.mobDescription : styles.webDescription}>
-            {groomingService?.description}
-          </Text> */}
-        </View>
-        <View>
-          <Image
-            source={require('../../assets/images/service/cat-grooming.jpg')}
-            style={mobile ? styles.mobImage : styles.webImage}
-          />
-        </View>
-        <View style={mobile ? styles.mobButton : styles.webButton}>
-          <SubmitButton value='Book Now' />
+      <View style={mobile ? styles.mobServicesForPetWrapper : styles.webServicesForPetWrapper}>
+        <View style={mobile ? styles.mobCard : styles.webCard}>
+          <View style={mobile ? styles.mobServices : styles.webServices}>
+            <Text style={mobile ? styles.mobLabel : styles.webLabel}>
+              Grooming
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Basic Bath & Brush: {'$'}25
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes a basic grooming package with a bath, brush, and nail trim.
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Standard Grooming: {'$'}40
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes a full grooming package with bath, brush, nail trim, and optional add-ons such as ear cleaning or flea treatments.
+            </Text>
+            <Text style={mobile ? styles.mobH3 : styles.webH3}>
+              Premium Grooming: {'$'}60
+            </Text>
+            <Text style={mobile ? styles.mobDescription : styles.webDescription}>
+              Includes a premium grooming expeirence with high-quality products, gentle handling, and extra pampering such as a fur massage or nail buffing.
+            </Text>
+          </View>
+          <View style={styles.imgWrapper}>
+            <Image
+              source={require('../../assets/images/service/cat-grooming.jpg')}
+              style={mobile ? styles.mobImage : styles.webImage}
+            />
+          </View>
+          <View style={mobile ? styles.mobButton : styles.webButton}>
+            <SubmitButton value='Book Now' />
+          </View>
         </View>
       </View>
     </View>
--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -8,52 +8,52 @@ import { sServiceCardData } from '../../utils/data';
 
 const Services = () => {
   const mobile = Platform.OS !== 'web';
-  const dogServices = sServiceCardData.find((pet) => pet.title === 'Dog')?.services || [];
-  const catServices = sServiceCardData.find((pet) => pet.title === 'Cat')?.services || [];
+  // const dogServices = sServiceCardData.find((pet) => pet.title === 'Dog')?.services || [];
+  // const catServices = sServiceCardData.find((pet) => pet.title === 'Cat')?.services || [];
 
-  const dogBoardingService = dogServices.find((service) => service.type === 'boarding') || {
-    type: '',
-    title: '',
-    price: 0,
-    description: '',
-  };
-  const dogDaycareService = dogServices.find((service) => service.type === 'daycare') || {
-    type: '',
-    title: '',
-    price: 0,
-    description: '',
-  };
-  const dogGroomingService = dogServices.find((service) => service.type === 'grooming') || {
-    type: '',
-    title: '',
-    price: 0,
-    description: '',
-  };
-  const dogTrainingService = dogServices.find((service) => service.type === 'training') || {
-    type: '',
-    title: '',
-    price: 0,
-    description: '',
-  };
+  // const dogBoardingService = dogServices.find((service) => service.type === 'boarding') || {
+  //   type: '',
+  //   title: '',
+  //   price: 0,
+  //   description: '',
+  // };
+  // const dogDaycareService = dogServices.find((service) => service.type === 'daycare') || {
+  //   type: '',
+  //   title: '',
+  //   price: 0,
+  //   description: '',
+  // };
+  // const dogGroomingService = dogServices.find((service) => service.type === 'grooming') || {
+  //   type: '',
+  //   title: '',
+  //   price: 0,
+  //   description: '',
+  // };
+  // const dogTrainingService = dogServices.find((service) => service.type === 'training') || {
+  //   type: '',
+  //   title: '',
+  //   price: 0,
+  //   description: '',
+  // };
 
-  const catBoardingService = catServices.find((service) => service.type === 'boarding') || {
-    type: '',
-    title: '',
-    price: 0,
-    description: '',
-  };
-  const catDaycareService = catServices.find((service) => service.type === 'daycare') || {
-    type: '',
-    title: '',
-    price: 0,
-    description: '',
-  };
-  const catGroomingService = catServices.find((service) => service.type === 'grooming') || {
-    type: '',
-    title: '',
-    price: 0,
-    description: '',
-  };
+  // const catBoardingService = catServices.find((service) => service.type === 'boarding') || {
+  //   type: '',
+  //   title: '',
+  //   price: 0,
+  //   description: '',
+  // };
+  // const catDaycareService = catServices.find((service) => service.type === 'daycare') || {
+  //   type: '',
+  //   title: '',
+  //   price: 0,
+  //   description: '',
+  // };
+  // const catGroomingService = catServices.find((service) => service.type === 'grooming') || {
+  //   type: '',
+  //   title: '',
+  //   price: 0,
+  //   description: '',
+  // };
 
   return (
     // <View style={mobile ? styles.mobWrapper : styles.webWrapper}>
@@ -93,18 +93,18 @@ const Services = () => {
         <Text style={mobile ? styles.mobH1 : styles.webH1}>Services for Dogs</Text>
         
         <DogServiceCards
-  boardingService={[dogBoardingService]}
-  daycareService={[dogDaycareService]}
-  groomingService={[dogGroomingService]}
-  trainingService={[dogTrainingService]}
+  // boardingService={[dogBoardingService]}
+  // daycareService={[dogDaycareService]}
+  // groomingService={[dogGroomingService]}
+  // trainingService={[dogTrainingService]}
         />
 
         {/* CAT SECTION */}
         <Text style={mobile ? styles.mobH1 : styles.webH1}>Services for Cats</Text>
         <CatServiceCards
-          boardingService={[catBoardingService]}
-          daycareService={[catDaycareService]}
-          groomingService={[catGroomingService]}
+          // boardingService={[catBoardingService]}
+          // daycareService={[catDaycareService]}
+          // groomingService={[catGroomingService]}
         />
       </View>
     </Container>
