e2001cc31083d2ffa87f7246815e431208eef20a
kyleochata <kyleochata@gmail.com>
2024-03-04 20:36:00 -0800

    save for pull

--- components/SearchBar/filterModal.tsx
+++ components/SearchBar/filterModal.tsx
@@ -1,11 +1,18 @@
 import { View, Text } from 'react-native'
-const filterModal = () => {
+import { Foundation } from '@expo/vector-icons'
+import styles from './style'
+const FilterModal = () => {
   return (
-    <View>
-      <View>Pet:</View>
-      <View>Service: </View>
-      <View>Price Range: </View>
+    <View style={styles.fMCon}>
+      <View style={styles.fMBorderArrow}>
+        <Foundation name="arrow-up" size={80} color="white" />
+      </View>
+      <View style={styles.fMIC}>
+        <View>Pet:</View>
+        <View>Service: </View>
+        <View>Price Range: </View>
+      </View>
     </View>
   )
 }
-export default filterModal
+export default FilterModal
